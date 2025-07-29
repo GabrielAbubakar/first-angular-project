b2df1301eddac8b891908fa7b324b2eb496de389
Gabriel Abubakar <gabrielogrima@gmail.com>
2025-07-29 12:56:19 +0100

    video 45 completed

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { TaskComponent } from "./task/task.component";
+import { title } from 'process';
 
 @Component({
   selector: 'app-tasks',
@@ -8,5 +9,35 @@ import { TaskComponent } from "./task/task.component";
   styleUrl: './tasks.component.scss'
 })
 export class TasksComponent {
-  @Input({ required: true })  name?: string;
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true })  name!: string;
+
+  tasks = [
+    {
+      id: 't1',
+      userId: 'u1',
+      title : 'Task 1',
+      summary: 'This is the first task',
+      dueDate: '2023-10-01',
+    },
+    {
+      id: 't2',
+      userId: 'u3',
+      title : 'Task 1',
+      summary: 'This is the first task',
+      dueDate: '2023-10-01',
+    },
+    {
+      id: 't3',
+      userId: 'u3',
+      title : 'Task 1',
+      summary: 'This is the first task',
+      dueDate: '2023-10-01',
+    },
+  ]
+
+  // Computed Property
+  get selectedUserTasks() {
+    return this.tasks.filter(task => task.userId === this.userId);
+  }
 }
